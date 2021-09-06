60e0fecf9f33c07eda2c65ac7a6c937160aef689
TheDoctor314 <darsh.rajan95@gmail.com>
2021-09-06 23:41:15 +0530

    Move private functions to the end

--- src/HashTable.h
+++ src/HashTable.h
@@ -72,107 +72,6 @@ class HashTable {
     size_t deleted_count_{0};
     size_t capacity_{0};
 
-   private:
-    size_t used_buckets_count() const { return size_ + deleted_count_; }
-    bool should_grow() const {
-        return ((used_buckets_count() + 1) * 100) >=
-               (capacity_ * load_factor_percent);
-    }
-
-    void rehash(size_t new_capacity) {
-        new_capacity = std::max(new_capacity, static_cast<size_t>(4));
-
-        auto old_capacity = capacity_;
-        auto* old_buckets = buckets_;
-
-        auto* new_buckets =
-            (Bucket*)std::malloc(sizeof(Bucket) * (new_capacity + 1));
-        if (!new_buckets) {
-            return;
-        }
-        __builtin_memset(new_buckets, 0, sizeof(Bucket) * (new_capacity + 1));
-
-        buckets_ = new_buckets;
-        capacity_ = new_capacity;
-        deleted_count_ = 0;
-
-        // sentinel pointer to mark end of line
-        buckets_[capacity_].end = true;
-
-        if (!old_buckets) return;
-
-        for (size_t i = 0; i < old_capacity; i++) {
-            // move from old table to new
-            auto& old_bucket = old_buckets[i];
-
-            // We insert only used objects, not deleted objects
-            if (old_bucket.used) {
-                insert_during_rehash(*old_bucket.slot());
-                old_bucket.slot()->~T();
-            }
-        }
-
-        std::free(old_buckets);
-    }
-
-    void insert_during_rehash(T& val) {
-        auto& bucket = lookup_for_writing(val);
-
-        /* We use the placement new syntax. This allows us to
-         * provide the address where the object should be constructed.*/
-        new (bucket.slot()) T(val);
-        bucket.used = true;
-    }
-
-    template <typename Pred>
-    Bucket* lookup_with_hash(unsigned hash, Pred predicate) const {
-        if (is_empty()) return nullptr;
-
-        auto index = hash % capacity_;
-        for (;;) {
-            auto& bucket = buckets_[index];
-
-            if (bucket.used && predicate(*bucket.slot())) return &bucket;
-
-            if (!bucket.used && !bucket.deleted) return nullptr;
-
-            // Linear probing
-            index = (index + 1) % capacity_;
-        }
-    }
-    const Bucket* lookup_for_reading(const T& value) const {
-        lookup_with_hash(TraitsForT::hash(value), [&value](auto& entry) {
-            return TraitsForT::equals(entry, value);
-        });
-    }
-
-    Bucket& lookup_for_writing(const T& value) {
-        if (should_grow()) {
-            rehash(capacity() * 2);
-        }
-
-        auto hash = TraitsForT::hash(value);
-        auto bucket_index = hash % capacity_;
-
-        /*We need to return the first empty bucket we come across or the bucket
-        which needs to be replaced*/
-        Bucket* first_empty_bucket = nullptr;
-        while (true) {
-            auto& bucket = buckets_[bucket_index];
-
-            if (bucket.used && TraitsForT::equals(*bucket.slot(), value))
-                return bucket;
-
-            if (!bucket.used) {
-                if (!first_empty_bucket) first_empty_bucket = &bucket;
-
-                if (!bucket.deleted) return *first_empty_bucket;
-            }
-
-            bucket_index = (bucket_index + 1) % capacity_;
-        }
-    }
-
    public:
     HashTable() = default;
     explicit HashTable(size_t capacity) { rehash(capacity); }
@@ -336,6 +235,107 @@ class HashTable {
         }
 
         return false;
+    }
+
+   private:
+    size_t used_buckets_count() const { return size_ + deleted_count_; }
+    bool should_grow() const {
+        return ((used_buckets_count() + 1) * 100) >=
+               (capacity_ * load_factor_percent);
+    }
+
+    void rehash(size_t new_capacity) {
+        new_capacity = std::max(new_capacity, static_cast<size_t>(4));
+
+        auto old_capacity = capacity_;
+        auto* old_buckets = buckets_;
+
+        auto* new_buckets =
+            (Bucket*)std::malloc(sizeof(Bucket) * (new_capacity + 1));
+        if (!new_buckets) {
+            return;
+        }
+        __builtin_memset(new_buckets, 0, sizeof(Bucket) * (new_capacity + 1));
+
+        buckets_ = new_buckets;
+        capacity_ = new_capacity;
+        deleted_count_ = 0;
+
+        // sentinel pointer to mark end of line
+        buckets_[capacity_].end = true;
+
+        if (!old_buckets) return;
+
+        for (size_t i = 0; i < old_capacity; i++) {
+            // move from old table to new
+            auto& old_bucket = old_buckets[i];
+
+            // We insert only used objects, not deleted objects
+            if (old_bucket.used) {
+                insert_during_rehash(*old_bucket.slot());
+                old_bucket.slot()->~T();
+            }
+        }
+
+        std::free(old_buckets);
+    }
+
+    void insert_during_rehash(T& val) {
+        auto& bucket = lookup_for_writing(val);
+
+        /* We use the placement new syntax. This allows us to
+         * provide the address where the object should be constructed.*/
+        new (bucket.slot()) T(val);
+        bucket.used = true;
+    }
+
+    template <typename Pred>
+    Bucket* lookup_with_hash(unsigned hash, Pred predicate) const {
+        if (is_empty()) return nullptr;
+
+        auto index = hash % capacity_;
+        for (;;) {
+            auto& bucket = buckets_[index];
+
+            if (bucket.used && predicate(*bucket.slot())) return &bucket;
+
+            if (!bucket.used && !bucket.deleted) return nullptr;
+
+            // Linear probing
+            index = (index + 1) % capacity_;
+        }
+    }
+    const Bucket* lookup_for_reading(const T& value) const {
+        lookup_with_hash(TraitsForT::hash(value), [&value](auto& entry) {
+            return TraitsForT::equals(entry, value);
+        });
+    }
+
+    Bucket& lookup_for_writing(const T& value) {
+        if (should_grow()) {
+            rehash(capacity() * 2);
+        }
+
+        auto hash = TraitsForT::hash(value);
+        auto bucket_index = hash % capacity_;
+
+        /*We need to return the first empty bucket we come across or the bucket
+        which needs to be replaced*/
+        Bucket* first_empty_bucket = nullptr;
+        while (true) {
+            auto& bucket = buckets_[bucket_index];
+
+            if (bucket.used && TraitsForT::equals(*bucket.slot(), value))
+                return bucket;
+
+            if (!bucket.used) {
+                if (!first_empty_bucket) first_empty_bucket = &bucket;
+
+                if (!bucket.deleted) return *first_empty_bucket;
+            }
+
+            bucket_index = (bucket_index + 1) % capacity_;
+        }
     }
 };
 }  // namespace hashfu
